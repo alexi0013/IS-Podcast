4dee97ed3e275ead8e4ec633557df487d029c539
alexi0013 <167550586+alexi0013@users.noreply.github.com>
2025-04-16 13:57:10 +0200

    Create Transcrib.py

---
+++ Transcrib.py
@@ -0,0 +1,16 @@
+import whisper
+import os
+
+# 🔽 1. Pfad zur Datei im Download-Ordner
+download_path = os.path.expanduser("/Users/alexisarvanitidis/Documents/MasterThesis/doweneedtheory.mp3")
+
+# 🔽 2. Whisper-Modell laden
+model = whisper.load_model("tiny") 
+
+# 3. Transkribieren
+result = model.transcribe(download_path)
+
+# 4. Transkript speichern
+output_file = os.path.expanduser("/Users/alexisarvanitidis/Documents/MasterThesis/episode18_transkript.txt")
+with open(output_file, "w", encoding="utf-8") as f:
+    f.write(result["text"])
